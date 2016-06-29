073a9b191d35e96c4e1e0d566d211561f874d2d2
Phoebe C <phoebecheung12@berkeley.edu>
2016-06-29 15:40:01 -0700

    adding ffs.py

---
+++ ffs.py
@@ -0,0 +1,44 @@
+def dont_try(n):
+	# DOESN'T WORK FOR BIG THINGS: Recursion overload.
+    """Return the nth element of up-down, seven-switch sequence.
+
+    >>> dont_try(7)
+    7
+    >>> dont_try(8)
+    6
+    >>> dont_try(15)
+    1
+    """
+    def switch():
+        if dont_try(n-1)-dont_try(n-2)==1:
+            return -1
+        if dont_try(n-1)-dont_try(n-2)==-1:
+            return 1
+    def no_switch():
+        if dont_try(n-1)-dont_try(n-2)==1:
+            return 1
+        if dont_try(n-1)-dont_try(n-2)==-1:
+            return -1
+    assert n>0, "Can't do this for non-positive integers"
+    assert n%1==0, "Can't do this for non-integers"
+    if n==1:
+        return 1
+    elif n == 2:
+        return 2
+    elif (n-1) % 7 ==0 or ('7' in str(n-1))==True:
+        return dont_try(n-1)+switch()
+    else:
+        return dont_try(n-1)+no_switch()
+
+def tiesto(hello):
+    """ 
+    Returns the highest power of two lower than 'hello.'
+    """
+    import math
+    if (math.log(hello)/math.log(2))%1==0:
+        return hello
+    else:
+        for i in range(1,hello):
+            if (math.log(i)/math.log(2))%1==0:
+                x=i
+    return x
