ebf8b0e7f08d472b3380d8f2660fcc71a2b5da02
Phoebe C <phoebecheung12@berkeley.edu>
2017-05-10 11:23:16 -0700

    C self teaching

---
+++ tutorialspoint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <float.h>
+
+int i, j, k;
+char c, ch;
+float f, salary;
+double d;
+
+// function declaration
+int func();
+
+int main(){
+	   printf("Storage size for float : %d \n", sizeof(float));
+   printf("Minimum float positive value: %E\n", FLT_MIN );
+   printf("Maximum float positive value: %E\n", FLT_MAX );
+   printf("Precision value: %d\n", FLT_DIG );
+   i=10;
+   j=11;
+   k=290;
+
+// function call
+   int x = func();
+
+   printf("this is an integer list: %d\n%d\n%d\n",i,j,k);
+   
+  // return 0;
+}
+
+//function definition: note it is after function call
+// a la forward declaration
+int func(){
+	return 0;
+}
