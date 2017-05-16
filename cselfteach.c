cb238902c347dd76f6b1eac0a7ee4d19929b8824
Phoebe C <phoebecheung12@berkeley.edu>
2017-05-16 13:42:23 -0700

    too much to explain to myself, no message

--- cselfteach.c
+++ cselfteach.c
@@ -2,6 +2,7 @@
 #include "csupport.c"
 #include <float.h>
 #include <math.h>
+# include <string.h>
 
 int i=10, j=11, k=290;
 int z = 19;
@@ -16,7 +17,9 @@ double d;
 // function declaration
 int func();
 //extern void write_extern(void);
-
+void hello();
+int marcus();
+int elizabeth();
 int main(){
 	   printf("Storage size for float : %d \n", sizeof(float));
    printf("Minimum float positive value: %E\n", FLT_MIN );//same thing as with %d
@@ -44,6 +47,19 @@ int main(){
    printf("this is an integer list: %d, %d, %d\n",i,j,k);
    printf("%d\n",x);
 
+// testing do... while   
+//below: the condition starts off unmet. But in spite of that,
+// the program runs once, because condition is evaluated at end.
+   int ster = 51;
+   do {printf("hi i'm %d\n",ster);} while(ster<20);
+
+   int ai=10;
+
+katherine(ai);
+print_stars_like_java();
+
+hello();
+
   // return 0;
 }
 
@@ -51,4 +67,49 @@ int main(){
 // a la forward declaration
 int func(){
 	return 0;
+}
+
+//for some reason, calling these void doesn't work
+
+int marcus(int x){
+  if (x==15){printf("leedleedlee\n");}
+
+}
+
+int katherine(int qq)   {  
+
+//the function after the keyword (LOOP) will run even if
+// if not sent by the goto.
+       do {LOOP:marcus(qq); 
+ 
+      if( qq == 15) {
+         /* skip the iteration */
+         qq = qq + 1;
+         goto LOOP;
+      }
+      
+      printf("value of qq: %d\n", qq);
+      qq++;
+   }while( qq < 20 );
+ }
+
+int print_stars_like_java(){
+// you must create a character array
+// and it must be long enough
+// also just declaring char str; won't work, because you can't
+   // add on to it (I'm pretty sure)
+   char str[10];
+   int k= 0;
+   char* luna = "*";
+   strcpy(str,luna);
+   printf("%s\n",str);
+   do {
+      strcat(str,luna);
+      k++;
+      puts(str);
+   } while (k<8);
+}
+
+void hello(){
+   printf("hi!\n");
 }
