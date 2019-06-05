834401d548bff3e2117fc73244f0d15c5f167fe1
Phoebe Cheung <phoebecheung12@berkeley.edu>
2019-06-04 22:27:17 -0700

    a label-clearing script necessitated by the tidyverse (I think)

---
+++ clear_labels.R
@@ -0,0 +1,15 @@
+# clears labels (tidyverse imposes on sets called by `data()`)
+
+clear.labels <- function(x) {
+  if(is.list(x)) {
+    for(i in 1 : length(x)) class(x[[i]]) <- setdiff(class(x[[i]]), 'labelled') 
+    for(i in 1 : length(x)) attr(x[[i]],"label") <- NULL
+  }
+  else {
+    class(x) <- setdiff(class(x), "labelled")
+    attr(x, "label") <- NULL
+  }
+  return(x)
+}
+
+# credit: Dominic Comtois on stackoverflow
